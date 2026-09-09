f20e8b494f31e9103c160739681b6e58f08ba8cc
hakeem <abdhakeem123abd@gmail.com>
2026-09-09 15:14:05 +0530

    fix: use dj_database_url.parse() to handle DATABASE_URL reliably

--- core/settings.py
+++ core/settings.py
@@ -79,12 +79,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+_DATABASE_URL = config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+    'default': dj_database_url.parse(
+        _DATABASE_URL,
         conn_max_age=600,
         conn_health_checks=True,
-        ssl_require=True,
+        ssl_require=_DATABASE_URL.startswith('postgres'),
     )
 }
 
