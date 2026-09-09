8a878c535020b7b6dbc96fcbc941cfcc75867264
hakeem <abdhakeem123abd@gmail.com>
2026-09-09 15:55:33 +0530

    fix: allow Render domain in ALLOWED_HOSTS, set DEBUG=False by default

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-rxg4km%cz^##-25h#1i3x=+g4--+ri2&7-z@)-1zswe$8kw#=!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
 
 
 # Application definition
