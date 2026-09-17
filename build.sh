3f7c0c0778292e63c78de09cf60cbb3cd64cab76
hakeem <abdhakeem123abd@gmail.com>
2026-09-17 17:59:43 +0530

    create superuser automatically during build process

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ python manage.py migrate --noinput
 echo "==> Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "==> Creating superuser..."
+python manage.py createsuperuser --noinput || true
+
 echo "==> Build complete!"
